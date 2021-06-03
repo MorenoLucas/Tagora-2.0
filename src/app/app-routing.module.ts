339ea8dbe5f1f066387187fd32178d660801a8e7
MorenoLucas <sl.lucasmoreno@gmail.com>
2021-06-03 17:31:30 +0200

    rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,35 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ChatComponent } from './pages/chat/chat.component';
+import { DetailEventComponent } from './pages/detail-event/detail-event.component';
+import { DetailProductComponent } from './pages/detail-product/detail-product.component';
+import { DetailTattooComponent } from './pages/detail-tattoo/detail-tattoo.component';
+import { EventsComponent } from './pages/events/events.component';
+import { HomepageComponent } from './pages/homepage/homepage.component';
+import { OptionsComponent } from './pages/options/options.component';
+import { ProductComponent } from './pages/product/product.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { TattooComponent } from './pages/tattoo/tattoo.component';
+import { UserPersonalExternoComponent } from './pages/user-personal-externo/user-personal-externo.component';
+import { UserPersonalComponent } from './pages/user-personal/user-personal.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomepageComponent },
+  { path: '/tattoos', component: TattooComponent },
+  { path: '/product', component: ProductComponent },
+  { path: '/chat', component: ChatComponent },
+  { path: '/events/detail/:id', component: DetailEventComponent },
+  { path: '/product/detail/:id', component: DetailProductComponent },
+  { path: '/tattoos/detail/:id', component: DetailTattooComponent },
+  { path: '/envents', component: EventsComponent },
+  { path: '/options', component: OptionsComponent },
+  { path: '/register', component: RegisterComponent },
+  { path: '/user', component: UserPersonalComponent },
+  { path: '/user/external', component: UserPersonalExternoComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
