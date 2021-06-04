fa3391860eac879a8b25eb9be6514ada8458c9ed
MorenoLucas <sl.lucasmoreno@gmail.com>
2021-06-04 11:01:29 +0200

    registro, html Jordi, logica Lucas

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import {
+  FormGroup,
+  FormControl,
+  FormBuilder,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
+  registerForm: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder, private db: AngularFirestore) {}
 
   ngOnInit(): void {
+    this.registerForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+      isTatuador: ['', Validators.required],
+    });
   }
 
+  async onSubmit() {
+    console.warn(this.registerForm.getRawValue());
+    const data = this.registerForm.getRawValue();
+    const idObject = await this.db.collection('Users').add(data);
+  }
 }
